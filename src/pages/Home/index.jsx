2d19f929e3c56d33c35345d9b88073d8335192d0
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2023-08-08 10:58:29 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -24,7 +24,7 @@ export function Home() {
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('https://api.github.com/users/rodrigorgtic');
+      const response = await fetch('https://api.github.com/users/alissonandrade2020');
       const data = await response.json();
       console.log("DADOS ===> ", data);
 
